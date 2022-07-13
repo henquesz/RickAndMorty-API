b739c5405b82a24c35d5d86dcc04dc27b2f841f0
henquesz <henquesvinicius@gmail.com>
2022-07-13 19:34:48 -0300

    Base do projeto - pokedex (conexão de api direta ao pokeapi)

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card>
+        <v-container>
+          <v-row>
+            <v-col cols="3" v-for="pokemon in pokemons.slice(0,100)" :key="pokemon.name">
+              <v-card>
+                <v-container>
+                  <v-row class="mx-0 d-flex justify-center">
+                    <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`" :alt="pokemon.name" width="80%">
+                  </v-row>
+                  <h2 class="text-center">{{get_name(pokemon)}}</h2>
+                </v-container>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+import axios from "axios";
+export default {
+  name: "App",
+
+  components: {},
+
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+
+  mounted() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods:{
+    get_id(pokemon){
+      return Number(pokemon.url.split("/")[6]);
+    },
+    get_name(pokemon){
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
