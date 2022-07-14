ab7415137a0995d32c29a29c2f1a3b2c0c18cc39
henquesz <henquesvinicius@gmail.com>
2022-07-13 21:15:18 -0300

    inclusão de cards no click.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
   <v-app>
     <v-container>
-        <v-container>
-          <v-text-field
-            v-model="search"
-            label="Pesquisar"
-            placeholder="Bulbassaur" outlined
-          ></v-text-field>
-          <v-row>
-            <v-col cols="3" v-for="pokemon in filtered_pokemons || filtered_species" :key="pokemon.name">
-              <v-card>
-                <v-container>
-                  <v-row class="mx-0 d-flex justify-center">
-                    <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`" :alt="pokemon.name" width="80%">
-                  </v-row>
-                  <h2 class="text-center">{{get_name(pokemon)}}</h2>
-                </v-container>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+      <v-container>
+        <v-text-field
+          dark
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Bulbassaur"
+          outlined
+          color="success"
+        ></v-text-field>
+        <v-row>
+          <v-col
+            cols="3"
+            v-for="pokemon in filtered_pokemons || filtered_species"
+            :key="pokemon.name"
+          >
+            <v-card @click="show_pokemon(get_id(pokemon))">
+              <v-container>
+                <v-row class="mx-0 d-flex justify-center">
+                  <img
+                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                      pokemon
+                    )}.png`"
+                    :alt="pokemon.name"
+                    width="80%"
+                  />
+                </v-row>
+                <h2 class="text-center">{{ get_name(pokemon) }}</h2>
+                <h3 class="text-center">Id: {{get_id(pokemon)}}</h3>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
+
+    <v-dialog v-model="show_dialog" width="700">
+      <v-card v-if="selected_pokemon">
+       <v-container>
+        <v-row class="d-flex align-center">
+          <v-col cols=4>
+            <img
+                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`"
+                    :alt="selected_pokemon.name"
+                    width="80%"
+                  />
+          </v-col>
+          <v-col cols=8>
+            <h1>
+              {{ get_name(selected_pokemon)}}
+            </h1>
+            <span>Altura:{{selected_pokemon.height}}</span>
+          </v-col>
+        </v-row>
+          {{selected_pokemon}}
+       </v-container>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -36,6 +73,8 @@ export default {
     return {
       pokemons: [],
       search: "",
+      show_dialog: false,
+      selected_pokemon: null,
     };
   },
 
@@ -46,21 +85,30 @@ export default {
         this.pokemons = response.data.results;
       });
   },
-  methods:{
-    get_id(pokemon){
+  methods: {
+    get_id(pokemon) {
       return Number(pokemon.url.split("/")[6]);
     },
-    get_name(pokemon){
+    get_name(pokemon) {
       return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     },
+    show_pokemon(id){
+       axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+      
+    },
   },
-  computed:{
+  computed: {
     filtered_pokemons() {
       return this.pokemons.filter((item) => {
         return item.name.includes(this.search);
       });
     },
-     filtered_species() {
+    filtered_species() {
       return this.pokemons.filter((item) => {
         return item.species.includes(this.search);
       });
@@ -71,8 +119,12 @@ export default {
 
 <style>
 #app {
-background: rgb(12,39,63);
-background: radial-gradient(circle, rgba(12,39,63,1) 49%, rgba(10,10,10,1) 100%)
+  background: rgb(12, 39, 63);
+  background: radial-gradient(
+      circle,
+      rgba(12, 39, 63, 1) 49%,
+      rgba(10, 10, 10, 1) 100%
+    )
     no-repeat center center fixed !important;
   -webkit-background-size: cover;
   -moz-background-size: cover;
